b1cbd744e484553291542754bd900f3b0fe63f29
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-04-26 13:16:46 +0200

    FunctionAppDockerVersions

--- src/Pulumi.Azure.Constants/FunctionApp/FunctionAppDockerVersions.cs
+++ src/Pulumi.Azure.Constants/FunctionApp/FunctionAppDockerVersions.cs
@@ -2,10 +2,11 @@
 namespace Pulumi.Azure.Constants
 {
     /// <summary>
-    /// Linux App Framework and version for the AppService, e.g. `DOCKER|mcr.microsoft.com/azure-functions/dotnet:3.0`.
+    /// All Function App Docker versions (Linux and Windows) which are supported.
     /// </summary>
-    public static class FunctionAppSiteConfigLinuxVersions
+    public static class FunctionAppDockerVersions
     {
+        #region Linux
         public const string DotNetV2 = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:2.0";
 
         public const string DotNetV3 = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:3.0";
@@ -15,5 +16,14 @@ namespace Pulumi.Azure.Constants
         public const string PowerShellV2 = "DOCKER|mcr.microsoft.com/azure-functions/powershell:2.0";
 
         public const string PythonV2 = "DOCKER|mcr.microsoft.com/azure-functions/python:2.0";
+        #endregion
+
+        #region Windows
+        public const string DotNetV2NanoServer1709 = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:2.0-nanoserver-1709";
+
+        public const string DotNetV2NanoServer1803 = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:2.0-nanoserver-1803";
+
+        public const string DotNetV2NanoServer1809 = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:2.0-nanoserver-1809";
+        #endregion
     }
 }
