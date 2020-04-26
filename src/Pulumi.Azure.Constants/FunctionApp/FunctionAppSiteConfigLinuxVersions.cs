40a2b55dc7d0f59210bf8554526c896393b0f697
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-04-26 13:13:05 +0200

    Add more Linux and Windows docker versions

---
+++ src/Pulumi.Azure.Constants/FunctionApp/FunctionAppSiteConfigLinuxVersions.cs
@@ -0,0 +1,19 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Pulumi.Azure.Constants
+{
+    /// <summary>
+    /// Linux App Framework and version for the AppService, e.g. `DOCKER|mcr.microsoft.com/azure-functions/dotnet:3.0`.
+    /// </summary>
+    public static class FunctionAppSiteConfigLinuxVersions
+    {
+        public const string DotNetV2 = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:2.0";
+
+        public const string DotNetV3 = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:3.0";
+
+        public const string NodeV2 = "DOCKER|mcr.microsoft.com/azure-functions/node:2.0";
+
+        public const string PowerShellV2 = "DOCKER|mcr.microsoft.com/azure-functions/powershell:2.0";
+
+        public const string PythonV2 = "DOCKER|mcr.microsoft.com/azure-functions/python:2.0";
+    }
+}
