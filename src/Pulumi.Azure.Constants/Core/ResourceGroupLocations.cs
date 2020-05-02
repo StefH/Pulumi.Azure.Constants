6db2a23c8298a993c6b112b194505de80e6d9eaf
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2020-05-02 10:33:28 +0200

    ResourceGroupLocations (#3)

---
+++ src/Pulumi.Azure.Constants/Core/ResourceGroupLocations.cs
@@ -0,0 +1,44 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Pulumi.Azure.Constants
+{
+    /// <summary>
+    /// The Azure Region where the Resource Group should exist.
+    /// </summary>
+    public static class ResourceGroupLocations
+    {
+        public const string AustraliaCentral = "australiacentral";
+        public const string AustraliaCentral2 = "australiacentral2";
+        public const string AustraliaEast = "australiaeast";
+        public const string AustraliaSouthEast = "australiasoutheast";
+        public const string BrazilSouth = "brazilsouth";
+        public const string CanadaCentral = "canadacentral";
+        public const string CanadaEast = "canadaeast";
+        public const string CentralIndia = "centralindia";
+        public const string CentralUS = "centralus";
+        public const string EastAsia = "eastasia";
+        public const string EastUS = "eastus";
+        public const string EastUS2 = "eastus2";
+        public const string FranceCentral = "francecentral";
+        public const string FranceSouth = "francesouth";
+        public const string JapanEast = "japaneast";
+        public const string JapanWest = "japanwest";
+        public const string KoreaCentral = "koreacentral";
+        public const string KoreaSouth = "koreasouth";
+        public const string NorthCentralUS = "northcentralus";
+        public const string NorthEurope = "northeurope";
+        public const string SouthAfricaNorth = "southafricanorth";
+        public const string SouthAfricaWest = "southafricawest";
+        public const string SouthCentralUS = "southcentralus";
+        public const string SouthEastAsia = "southeastasia";
+        public const string SouthIndia = "southindia";
+        public const string UAECentral = "uaecentral";
+        public const string UAENorth = "uaenorth";
+        public const string UKSouth = "uksouth";
+        public const string UKWest = "ukwest";
+        public const string WestCentralUS = "westcentralus";
+        public const string WestEurope = "westeurope";
+        public const string WestIndia = "westindia";
+        public const string WestUS = "westus";
+        public const string WestUS2 = "westus2";
+    }
+}
